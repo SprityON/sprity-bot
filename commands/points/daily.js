0f989d166c4177078057a0ab901460e0bf17205a
SprityON <71904569+SprityON@users.noreply.github.com>
2021-03-14 16:30:44 +0100

    test3...

--- commands/points/daily.js
+++ commands/points/daily.js
@@ -26,7 +26,7 @@ module.exports.command = {
             let prevDate
             let nextDate = currentDate.clone().add(1, 'day')
 
-            if (result.length == 0) {
+            if (result.length == 0 || !result) {
                 console.log(result[0])
                 query(`INSERT INTO currency_times (member_id) VALUES ( '${msg.member.id}')`)
             } else {
