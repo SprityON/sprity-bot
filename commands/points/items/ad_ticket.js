90810e8fb6fc04a68d6de4016a0b1d07bd217f26
SprityON <71904569+SprityON@users.noreply.github.com>
2021-03-18 12:25:51 +0100

    fixed

--- commands/points/items/ad_ticket.js
+++ commands/points/items/ad_ticket.js
@@ -1,3 +1,5 @@
+const { Functions } = require("../../../variables")
+
 module.exports.info = {
     name: 'Advertising Ticket',
     category: 'points',
@@ -14,12 +16,13 @@ module.exports.info = {
 
 module.exports.command = {
     async execute(msg, args, amount, client) {
-        msg.member.roles.cache.get('818558804617986089') 
-        ? msg.channel.send(`**${msg.author.username}**, you are already using **1** ticket.`) 
-        : (function() {
+        let role = msg.member.roles.cache.get('818558804617986089') 
+        if(role) {
+            msg.channel.send(`**${msg.author.username}**, you are already using **1** ticket.`) 
+        } else {
             Functions.changeInventory(amount, 'ad_ticket', msg)
             msg.member.roles.add('818558804617986089')
             msg.channel.send(`**${msg.author.username}**, you can now advertise **once** in <#818558571410096148>.`)
-        })
+        }
     }
 }
