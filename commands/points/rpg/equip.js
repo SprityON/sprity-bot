5d776c7f3e39ce73435441b4b509a18ce33fe938
SprityON <71904569+SprityON@users.noreply.github.com>
2021-03-27 13:24:09 +0100

    equip better

--- commands/points/rpg/equip.js
+++ commands/points/rpg/equip.js
@@ -20,35 +20,42 @@ module.exports.command = {
             let result = data[0][0]
             let inventory = JSON.parse(result.inventory)
             
+            if (!args[1]) return msg.channel.send(`You have to provide a item id!`)
             let item_id = args[1].toLowerCase()
-            if (!item_id) return msg.channel.send(`You have to provide a item id!`)
             let equippedWeapon = inventory.find(item => item.equipped === true)
 
+            let alreadyEquippedItem
+
             if (equippedWeapon) { if (equippedWeapon.equipped === true) {
-                return msg.channel.send(`You already have an equipped item! Unequip with \`$rpg unequip <item id>\``)
-            } } else {
-                let item = inventory.find(item => item.name === item_id)
-                if (!item) return msg.channel.send(`Item \`${item_id}\` does not exist!`)
-
-                inventory = JSON.parse(result.inventory)
-
-                let inventoryItems = `[`
-                let i = 0
-                for (let item of inventory) {
-                    i++
-
-                    if (item.name !== item_id) {
-                        inventoryItems += `{ \"name\": \"${item.name}\", \"amount\": ${item.amount}, \"equipped\": ${item.equipped}, \"category\": \"weapons\" }, `
-                    }
+                if (equippedWeapon.name === item_id) {
+                    return msg.channel.send(`You already have this item equipped! Unequip with \`$rpg unequip <item id>\``)
+                } else {
+                    alreadyEquippedItem = equippedWeapon
                 }
-                inventoryItems += `{ \"name\": \"${item_id}\", \"amount\": 1, \"equipped\": true, \"category\": \"weapons\" } `
+            } }
 
-                inventoryItems += ']'
+            let item = inventory.find(item => item.name === item_id)
+            if (!item) return msg.channel.send(`Item \`${item_id}\` does not exist!`)
 
-                msg.channel.send(`You have equipped item **${item_id}**!`)
+            inventory = JSON.parse(result.inventory)
 
-                query(`UPDATE members_rpg SET inventory = '${inventoryItems}' WHERE member_id = ${msg.member.id}`)
+            let inventoryItems = `[`
+            let i = 0
+            for (let item of inventory) {
+                i++
+                if (item.name === alreadyEquippedItem.name) {
+                    inventoryItems += `{ \"name\": \"${item.name}\", \"amount\": ${item.amount}, \"equipped\": false, \"category\": \"weapons\" }, `
+                } else if (item.name !== item_id) {
+                    inventoryItems += `{ \"name\": \"${item.name}\", \"amount\": ${item.amount}, \"equipped\": ${item.equipped}, \"category\": \"weapons\" }, `
+                }
             }
+            inventoryItems += `{ \"name\": \"${item_id}\", \"amount\": 1, \"equipped\": true, \"category\": \"weapons\" } `
+
+            inventoryItems += ']'
+
+            msg.channel.send(`You have equipped item **${item_id}**!`)
+
+            query(`UPDATE members_rpg SET inventory = '${inventoryItems}' WHERE member_id = ${msg.member.id}`)
         })
     }
 }
