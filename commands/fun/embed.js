d8e612a9e83f97d634bd732f9a53a6634a0f8a5a
SprityON <71904569+SprityON@users.noreply.github.com>
2021-03-28 21:56:53 +0200

    last embed

--- commands/fun/embed.js
+++ commands/fun/embed.js
@@ -22,17 +22,17 @@ module.exports.command = {
         // .setColor(`#ffaa00`)
         // )
 
-        msg.channel.send(new Discord.MessageEmbed()
-        .setTitle(`Rules`)
-        .setDescription(`Obviously, just like in any other server, there are <#380724759740153866> which you have to follow.\n\nThere are many great perks you can get in the server, and by disobeying the rules your chance of losing these perks will grow bigger.`)
-        .setColor(embedcolor)
-        )
-
         // msg.channel.send(new Discord.MessageEmbed()
-        // .setTitle(`Customized Content`)
-        // .setDescription(`We have many customized stuff in this server! Read for more info`)
-        // .addField(`Points`, `In this server, we have a "currency" called \`points\`.\nWith Points, you can buy your own color role, advertisement tickets (in <#818558571410096148>), change your nickname and much, much more!\n\nQ: Okay sure, having a Points system is fine. But how do you get Points?\nA: You can get Points by either doing: \`$daily\` \`$weekly\` \`$monthly\` OR by creating an RPG account and get Points that way! (\`$rpg start\`)`)
-        // .setColor(`00ff00`)
+        // .setTitle(`Rules`)
+        // .setDescription(`Obviously, just like in any other server, there are <#380724759740153866> which you have to follow.\n\nThere are many great perks you can get in the server, and by disobeying the rules your chance of losing these perks will grow bigger.`)
+        // .setColor(embedcolor)
         // )
+
+        msg.channel.send(new Discord.MessageEmbed()
+        .setTitle(`Customized Content`)
+        .setDescription(`We have many customized stuff in this server! Read for more info`)
+        .addField(`Points`, `In this server, we have a "currency" called \`points\`.\nWith Points, you can buy your own color role, advertisement tickets (in <#818558571410096148>), change your nickname and much, much more!\n\nQ: Okay sure, having a Points system is fine. But how do you get Points?\nA: You can get Points by either doing: \`$daily\` \`$weekly\` \`$monthly\` OR by creating an RPG account and get Points that way! (\`$rpg start\`)`)
+        .setColor(`00ff00`)
+        )
     }
 }
