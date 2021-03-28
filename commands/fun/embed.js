96ad4cf65f130f0c54f046c2c678278695c93c57
SprityON <71904569+SprityON@users.noreply.github.com>
2021-03-28 21:56:24 +0200

    embed 2

--- commands/fun/embed.js
+++ commands/fun/embed.js
@@ -16,17 +16,17 @@ module.exports.info = {
 
 module.exports.command = {
     execute(msg, args, amount, client) {
-        msg.channel.send(new Discord.MessageEmbed()
-        .setTitle(`About The Official Sprity Server`)
-        .setDescription(`Well, hello there! In this channel you will read about all the stuff that our server provides.\n\nInterested? Then check out the following sections!`)
-        .setColor(`#ffaa00`)
-        )
-
         // msg.channel.send(new Discord.MessageEmbed()
-        // .setTitle(`Rules`)
-        // .setDescription(`Obviously, just like in any other server, there are <#380724759740153866> which you have to follow.\n\nThere are many great perks you can get in the server, and by disobeying the rules your chance of losing these perks will grow bigger.`)
-        // .setColor(embedcolor)
+        // .setTitle(`About The Official Sprity Server`)
+        // .setDescription(`Well, hello there! In this channel you will read about all the stuff that our server provides.\n\nInterested? Then check out the following sections!`)
+        // .setColor(`#ffaa00`)
         // )
+
+        msg.channel.send(new Discord.MessageEmbed()
+        .setTitle(`Rules`)
+        .setDescription(`Obviously, just like in any other server, there are <#380724759740153866> which you have to follow.\n\nThere are many great perks you can get in the server, and by disobeying the rules your chance of losing these perks will grow bigger.`)
+        .setColor(embedcolor)
+        )
 
         // msg.channel.send(new Discord.MessageEmbed()
         // .setTitle(`Customized Content`)
