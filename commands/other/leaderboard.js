8113126a5917603fa3d37200f29fd7a78af6eae8
SprityON <71904569+SprityON@users.noreply.github.com>
2021-04-02 00:36:20 +0200

    lb add

--- commands/other/leaderboard.js
+++ commands/other/leaderboard.js
@@ -39,11 +39,7 @@ module.exports.command = {
     
                     query(`SELECT * FROM members ORDER BY points LIMIT 5`, data => {
                         let members = data[0]
-                        let boolThisMember1 = ''
-                        let boolThisMember2 = ''
-                        let boolThisMember3 = ''
-                        let boolThisMember4 = ''
-                        let boolThisMember5 = ''
+                        let boolThisMember = ''
     
                         let topChatter = msg.guild.members.cache.find(member => member.displayName === memberList[0].name)
                         let firstplacetext = `${topChatter} is 1st place with over ***${memberList[0].points}*** points!`
@@ -51,44 +47,103 @@ module.exports.command = {
                         let i
                         for (i = 0; i < memberList.length; i++) {
                             const member = memberList[i];
-    
-                            if (member.name == msg.member.displayName) {
-                                switch (i) {
-                                    case 0: boolThisMember1 = '(YOU)'; firstplacetext = `You are 1st place with over ***${memberList[0].points}*** points!`; break;
-                                    case 1: boolThisMember2 = '(YOU)'; break;
-                                    case 2: boolThisMember3 = '(YOU)'; break;
-                                    case 3: boolThisMember4 = '(YOU)'; break;
-                                    case 4: boolThisMember5 = '(YOU)'; break;
+
+                            if (i < memberList.length) {
+                                if (member.name == msg.member.displayName) {
+                                    boolThisMember = '(YOU)'
                                 }
                             }
                         }
-    
-                        // embed for leaderboard
+
                         let embed = new Discord.MessageEmbed()
-                        .setTitle(`Leaderboard`)
+                        .addField(`\u200b`, `**Top 5 Richest Members [All-Time]\n**`)
+                        for (i = 0; i < memberList.length; i++) {
+                            embed.addField(`**${i+1}. ${memberList[i].name}**`, `Points: ${memberList[i].points}`)
+                        }
+
+                        // embed for leaderboard
+                        embed.setTitle(`Leaderboard`)
                         .setDescription(`${firstplacetext}`)
-                        .addField(`\u200b`, `**Top 5 Richest [All-Time]\n**`)
-                        .addField(`**1. ${memberList[0].name} ${boolThisMember1}**`,`Points: ${memberList[0].points}`)
-                        .addField(`**2. ${memberList[1].name} ${boolThisMember2}**`,`Points: ${memberList[1].points}`)
-                        .addField(`**3. ${memberList[2].name} ${boolThisMember3}**`,`Points: ${memberList[2].points}`)
-                        .addField(`**4. ${memberList[3].name} ${boolThisMember4}**`,`Points: ${memberList[3].points}`)
-                        .addField(`**5. ${memberList[4].name} ${boolThisMember5}**`,`Points: ${memberList[4].points}`)
                         .setColor(embedcolor)
-    
+
                         // if member is not in top 5, then add new field
                         if (!memberList.find(member => member.name === msg.member.displayName)) {
-    
+
                             let index
                             for (index = 0; index < members.length; index++) {
                                 const member = members[index];
                                 
                                 if (member.member_id == msg.member.id) {
-                                    let you = msg.guild.members.cache.find(member => member.id === `${thisMember.member_id}`)
+                                    let you = msg.guild.members.cache.find(member => member.id === `${thisMember[0].member_id}`)
                                     embed.addField(`\u200b`, `**${index + 1}. ${you.displayName} (YOU)**\nPoints: ${thisMember[0].points}`)
                                 }
                             }
                         }
-    
+
+                        msg.channel.send(embed)
+                    })
+                })
+            })
+        } else if (args[0] === 'gold' || args[0] === 'rpg') {
+            query(`SELECT * FROM members_rpg ORDER BY gold DESC LIMIT 5`, data => {
+                let memberList = []
+                data[0].forEach(row => {
+                    let member = msg.guild.members.cache.find(member => member.id === row.member_id)
+                    if (!member) { 
+                        memberList.push({"name": `Member not found`, "gold": `NULL`})
+                        return
+                    } else {
+                        memberList.push({"name": `${member.displayName}`, "gold": `${row.gold}`})
+                    }
+                });
+
+                query(`SELECT * FROM members WHERE member_id = ${msg.member.id}`, data => {
+                    let thisMember = data[0]
+
+                    query(`SELECT * FROM members_rpg ORDER BY gold LIMIT 5`, data => {
+                        let members = data[0]
+                        let boolThisMember = ''
+
+                        let goldEmoji = msg.guild.emojis.cache.find(e => e.name === 'gold')
+                        let topChatter = msg.guild.members.cache.find(member => member.displayName === memberList[0].name)
+                        let firstplacetext = `${topChatter} is 1st place with over ${goldEmoji} ***${memberList[0].gold}*** gold!`
+
+                        let i
+                        for (i = 0; i < memberList.length; i++) {
+                            const member = memberList[i];
+
+                            if (i < memberList.length) {
+                                if (member.name == msg.member.displayName) {
+                                    boolThisMember = '(YOU)'
+                                }
+                            }
+                        }
+
+                        let embed = new Discord.MessageEmbed()
+                        .addField(`\u200b`, `**Richest RPG Players [All-Time]\n**`)
+                        for (i = 0; i < memberList.length; i++) {
+                            embed.addField(`**${i+1}. ${memberList[i].name}**`, `Gold: ${memberList[i].gold}`)
+                        }
+
+                        // embed for leaderboard
+                        embed.setTitle(`RPG Leaderboard`)
+                        .setDescription(`${firstplacetext}`)
+                        .setColor(embedcolor)
+
+                        // if member is not in top 5, then add new field
+                        if (!memberList.find(member => member.name === msg.member.displayName)) {
+
+                            let index
+                            for (index = 0; index < members.length; index++) {
+                                const member = members[index];
+                                
+                                if (member.member_id == msg.member.id) {
+                                    let you = msg.guild.members.cache.find(member => member.id === `${thisMember[0].member_id}`)
+                                    embed.addField(`\u200b`, `**${index + 1}. ${you.displayName} (YOU)**\nGold: ${thisMember[0].gold}`)
+                                }
+                            }
+                        }
+
                         msg.channel.send(embed)
                     })
                 })
@@ -111,11 +166,7 @@ module.exports.command = {
 
                     query(`SELECT * FROM members ORDER BY messages LIMIT 5`, data => {
                         let members = data[0]
-                        let boolThisMember1 = ''
-                        let boolThisMember2 = ''
-                        let boolThisMember3 = ''
-                        let boolThisMember4 = ''
-                        let boolThisMember5 = ''
+                        let boolThisMember = ''
 
                         let topChatter = msg.guild.members.cache.find(member => member.displayName === memberList[0].name)
                         let firstplacetext = `${topChatter} is 1st place with over ***${memberList[0].messages}*** messages!`
@@ -124,27 +175,22 @@ module.exports.command = {
                         for (i = 0; i < memberList.length; i++) {
                             const member = memberList[i];
 
-                            if (member.name == msg.member.displayName) {
-                                switch (i) {
-                                    case 0: boolThisMember1 = '(YOU)'; firstplacetext = `You are 1st place with over ***${memberList[0].messages}*** messages!`; break;
-                                    case 1: boolThisMember2 = '(YOU)'; break;
-                                    case 2: boolThisMember3 = '(YOU)'; break;
-                                    case 3: boolThisMember4 = '(YOU)'; break;
-                                    case 4: boolThisMember5 = '(YOU)'; break;
+                            if (i < memberList.length) {
+                                if (member.name == msg.member.displayName) {
+                                    boolThisMember = '(YOU)'
                                 }
                             }
                         }
 
-                        // embed for leaderboard
                         let embed = new Discord.MessageEmbed()
-                        .setTitle(`Leaderboard`)
+                        .addField(`\u200b`, `**Most Active Players [This Week]\n**`)
+                        for (i = 0; i < memberList.length; i++) {
+                            embed.addField(`**${i+1}. ${memberList[i].name}**`, `Messages: ${memberList[i].messages}`)
+                        }
+
+                        // embed for leaderboard
+                        embed.setTitle(`Leaderboard`)
                         .setDescription(`${firstplacetext}`)
-                        .addField(`\u200b`, `**Top 5 Chatters [This Week]\n**`)
-                        .addField(`**1. ${memberList[0].name} ${boolThisMember1}**`,`Messages: ${memberList[0].messages}`)
-                        .addField(`**2. ${memberList[1].name} ${boolThisMember2}**`,`Messages: ${memberList[1].messages}`)
-                        .addField(`**3. ${memberList[2].name} ${boolThisMember3}**`,`Messages: ${memberList[2].messages}`)
-                        .addField(`**4. ${memberList[3].name} ${boolThisMember4}**`,`Messages: ${memberList[3].messages}`)
-                        .addField(`**5. ${memberList[4].name} ${boolThisMember5}**`,`Messages: ${memberList[4].messages}`)
                         .setColor(embedcolor)
 
                         // if member is not in top 5, then add new field
