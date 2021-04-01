a3f79054e3c4cb010d46ef10cec135be375a4f81
SprityON <71904569+SprityON@users.noreply.github.com>
2021-04-01 22:55:24 +0200

    test

--- commands/utils/private-channel.js
+++ commands/utils/private-channel.js
@@ -17,90 +17,92 @@ module.exports.info = {
 
 module.exports.command = {
     async execute(msg, args, amount, client) {
-        msg.channel.send(`Please specify an argument. Allowed arguments are:\n\`create (to create a private channel)\`\n\`invite <member>\`\n\`remove <member>\`\n\`remove (to delete your private channel)\`\n\n*Type \`cancel\` to cancel*`)
 
-        filter = m => m.author.id === msg.author.id
+        query(`SELECT * FROM private_channels WHERE member_id = ${msg.member.id}`, data => {
+            if (!data[0][0]) {
+                msg.channel.send(`Seems like you do not have a private channel yet!\nA private channel costs \`10,000\` points. Want to buy one? (y/n)`)
+                msg.channel.awaitMessages(filter, {max: 1, time: 60000})
+                .then(collected => {
+                    let cont = collected.first().content.toLowerCase()
+                    if (cont === 'y' || cont === 'yes') {
+                        query(`SELECT * FROM members WHERE member_id = ${msg.member.id}`, data => {
+                            let result = data[0][0]
+                            if (result.points < 10000) return msg.channel.send(`You do not have enough points to create a private channel!`)
 
-        await msg.channel.awaitMessages(filter, {max: 1, time: 60000})
-        .then(collected => {
-            if (collected.first().content.toLowerCase() !== 'cancel') {
-
-                let content = collected.first().content.toLowerCase()
-    
-                query(`SELECT * FROM private_channels WHERE member_id = ${msg.member.id}`, data => {
-                    console.log(data[0])
-                    if (!data[0][0]) {
-                        msg.channel.send(`Seems like you do not have a private channel yet!\n\nA private channel costs \`10,000\` points. Want to buy one? (y/n)`)
-                        msg.channel.awaitMessages(filter, {max: 1, time: 60000})
-                        .then(collected => {
-                            let cont = collected.first().content.toLowerCase()
-                            if (cont === 'y' || cont === 'yes') {
-                                query(`SELECT * FROM members WHERE member_id = ${msg.member.id}`, data => {
-                                    let result = data[0][0]
-                                    if (result.points < 10000) return msg.channel.send(`You do not have enough points to create a private channel!`)
-
-                                    msg.guild.channels.create(`${msg.author.username.toLowerCase()}'s private channel`).then(channel => {
-                                        channel.setParent('827185165985906729')
-                                        
-                                        channel.updateOverwrite(msg.guild.id, {
-                                            VIEW_CHANNEL: false
-                                        })
-                            
-                                        channel.updateOverwrite(msg.member.id, {
-                                            VIEW_CHANNEL: true
-                                        })
-    
-                                        query(`UPDATE members SET points = ${result.points - 10000} WHERE member_id = ${msg.member.id}`)
-                                        query(`INSERT INTO private_channels (member_id, channel_id) VALUES (${msg.member.id}, ${channel.id}) `)
-    
-                                        msg.channel.send(`Creation successful! Check out your private channel: ${channel}`)
-                                        channel.send(new Discord.MessageEmbed().setColor(embedcolor)
-                                        .setTitle(`Welcome to your private channel, ${msg.author.username}!`)
-                                        .setDescription(`**Invite/Remove a member from your channel**\n\`$use private_channel, and after that: invite <member>\` let a member join your private channel\n\`$use private-channel, and after that: remove <member>\` remove a member from your private channel\n\nRemove your channel by doing: \`$use private-channel. and after that: remove\``))
-                                    })
+                            msg.guild.channels.create(`${msg.author.username.toLowerCase()}'s private channel`).then(channel => {
+                                channel.setParent('827185165985906729')
+                                
+                                channel.updateOverwrite(msg.guild.id, {
+                                    VIEW_CHANNEL: false
+                                })
+                    
+                                channel.updateOverwrite(msg.member.id, {
+                                    VIEW_CHANNEL: true
                                 })
 
-                            } else return msg.channel.send(`Cancelled!`)
-                        })
-                    }
-                    const privateChannel = msg.guild.channels.cache.find(channel => channel.permissionOverwrites.find(overwrite => overwrite.id === msg.member.id))
+                                query(`UPDATE members SET points = ${result.points - 10000} WHERE member_id = ${msg.member.id}`)
+                                query(`INSERT INTO private_channels (member_id, channel_id) VALUES (${msg.member.id}, ${channel.id}) `)
 
-                    if (content.startsWith('invite')) {
-                        let mentioned = collected.first().mentions.members.first()
-                        if (mentioned) {
-                            privateChannel.updateOverwrite(mentioned.id, {
-                                VIEW_CHANNEL: true
+                                msg.channel.send(`Creation successful! Check out your private channel: ${channel}`)
+                                channel.send(new Discord.MessageEmbed().setColor(embedcolor)
+                                .setTitle(`Welcome to your private channel, ${msg.author.username}!`)
+                                .setDescription(`**Invite/Remove a member from your channel**\n\`$use private_channel, and after that: invite <member>\` let a member join your private channel\n\`$use private-channel, and after that: remove <member>\` remove a member from your private channel\n\nRemove your channel by doing: \`$use private-channel. and after that: remove\``))
                             })
-                            msg.channel.send(`You invited ${mentioned.displayName} to your private channel!`)
-                        } 
-                    }
-                    else if (content.startsWith('remove')) {
-                        if (content.includes('<@>')) {
+                        })
+
+                    } else return msg.channel.send(`Cancelled!`)
+                })
+            } else {
+                msg.channel.send(`Please specify an argument. Allowed arguments are:\n\`create (to create a private channel)\`\n\`invite <member>\`\n\`remove <member>\`\n\`remove (to delete your private channel)\`\n\n*Type \`cancel\` to cancel*`)
+
+                filter = m => m.author.id === msg.author.id
+    
+                await msg.channel.awaitMessages(filter, {max: 1, time: 60000})
+                .then(collected => {
+                    if (collected.first().content.toLowerCase() !== 'cancel') {
+    
+                        let content = collected.first().content.toLowerCase()
+            
+                            
+                            const privateChannel = msg.guild.channels.cache.find(channel => channel.permissionOverwrites.find(overwrite => overwrite.id === msg.member.id))
+    
+                            if (content.startsWith('invite')) {
                                 let mentioned = collected.first().mentions.members.first()
-                                
                                 if (mentioned) {
                                     privateChannel.updateOverwrite(mentioned.id, {
-                                        VIEW_CHANNEL: false
+                                        VIEW_CHANNEL: true
                                     })
-                                    return msg.channel.send(`You removed ${mentioned.displayName} from your private channel!`)
+                                    msg.channel.send(`You invited ${mentioned.displayName} to your private channel!`)
+                                } 
+                            }
+                            else if (content.startsWith('remove')) {
+                                if (content.includes('<@>')) {
+                                        let mentioned = collected.first().mentions.members.first()
+                                        
+                                        if (mentioned) {
+                                            privateChannel.updateOverwrite(mentioned.id, {
+                                                VIEW_CHANNEL: false
+                                            })
+                                            return msg.channel.send(`You removed ${mentioned.displayName} from your private channel!`)
+                                        } else {
+                                            return msg.channel.send(`You did not mention a member to remove!`)
+                                        }
+                                    
                                 } else {
-                                    return msg.channel.send(`You did not mention a member to remove!`)
+                                    msg.channel.send(`Are you sure you want to remove your private channel? (y/n)`)
+                                    msg.channel.awaitMessages(filter, {max: 1, time: 60000})
+                                    .then(collected => {
+                                        if (collected.first().content.toLowerCase() === 'y' || collected.first().content.toLowerCase() === 'yes' ) {
+                                            privateChannel.delete()
+                                            return msg.channel.send(`Your private channel was removed`)
+                                        } else return msg.channel.send(`Your private channel was not removed.`)
+                                    })
                                 }
-                            
-                        } else {
-                            msg.channel.send(`Are you sure you want to remove your private channel? (y/n)`)
-                            msg.channel.awaitMessages(filter, {max: 1, time: 60000})
-                            .then(collected => {
-                                if (collected.first().content.toLowerCase() === 'y' || collected.first().content.toLowerCase() === 'yes' ) {
-                                    privateChannel.delete()
-                                    return msg.channel.send(`Your private channel was removed`)
-                                } else return msg.channel.send(`Your private channel was not removed.`)
-                            })
-                        }
-                    }
+                            }
+                        
+                    } else return msg.channel.send('Cancelled!')
                 })
-                
-            } else return msg.channel.send('Cancelled!')
+            }
         })
     }
 }
