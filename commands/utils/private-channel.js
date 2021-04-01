42431cbde95f09ba44be25dc2c8c742c5b0fd8b0
SprityON <71904569+SprityON@users.noreply.github.com>
2021-04-01 23:36:14 +0200

    more features for pc

--- commands/utils/private-channel.js
+++ commands/utils/private-channel.js
@@ -34,6 +34,8 @@ module.exports.command = {
 
                             msg.guild.channels.create(`${msg.author.username.toLowerCase()}'s private channel`).then(channel => {
                                 channel.setParent('827185165985906729')
+
+                                channel.setTopic(`Owner: ${msg.author.username}`)
                                 
                                 channel.updateOverwrite(msg.guild.id, {
                                     VIEW_CHANNEL: false
@@ -56,7 +58,7 @@ module.exports.command = {
                     } else return msg.channel.send(`Cancelled!`)
                 })
             } else {
-                msg.channel.send(`Please specify an argument. Allowed arguments are:\n\`invite <member>\`\n\`remove <member>\`\n\`remove (to delete your private channel)\`\n\n*Type \`cancel\` to cancel*`)
+                msg.channel.send(`Please specify an argument. Allowed arguments are:\n\`change <topic|name>\`\n\`invite <member>\`\n\`remove <member>\`\n\`remove (to delete your private channel)\`\n\n*Type \`cancel\` to cancel*`)
     
                 msg.channel.awaitMessages(filter, {max: 1, time: 60000})
                 .then(collected => {
@@ -73,7 +75,7 @@ module.exports.command = {
                                     VIEW_CHANNEL: true
                                 })
                                 msg.channel.send(`You invited ${mentioned.displayName} to your private channel!`)
-                            } 
+                            } else return msg.channel.send(`To invite a member, mention them!`)
                         }
                         else if (content.startsWith('remove')) {
                             if (content.includes('<@>')) {
@@ -98,6 +100,27 @@ module.exports.command = {
                                         return msg.channel.send(`Your private channel was removed`)
                                     } else return msg.channel.send(`Your private channel was not removed.`)
                                 })
+                            }
+                        } else if (content.startsWith(`change`)) {
+                            let contentArgs = content.split(/ +/).shift()
+
+                            switch (contentArgs) {
+                                case 'description' || 'topic': 
+                                    msg.channel.send(`What would you like your channel topic to be?`)
+                                    msg.channel.awaitMessages(filter, {max: 1, time: 300000})
+                                    .then(collected => {
+                                        privateChannel.setTopic(collected.first().content)
+                                        return msg.channel.send(`Your channel topic has been changed.`)
+                                    })
+                                break
+                                case 'name': 
+                                    msg.channel.send(`What would you like your channel name to be?`)
+                                    msg.channel.awaitMessages(filter, {max: 1, time: 300000})
+                                    .then(collected => {
+                                        privateChannel.setName(collected.first().content)
+                                        return msg.channel.send(`Your channel name has been changed.`)
+                                    })
+                                break
                             }
                         }
                         
