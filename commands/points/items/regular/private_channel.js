3060ee390419f7fc86ab4e9686405fd482004921
SprityON <71904569+SprityON@users.noreply.github.com>
2021-04-01 18:05:08 +0200

    hm

--- commands/points/items/regular/private_channel.js
+++ commands/points/items/regular/private_channel.js
@@ -66,7 +66,7 @@ module.exports.command = {
                         .then(collected => {
                             if (collected.first().content.toLowerCase() === 'y' || collected.first().content.toLowerCase() === 'yes' ) {
                                 privateChannel.delete()
-                            } else status = [false, `Your private channel was not removed.`]
+                            } else msg.channel.send(`Your private channel was not removed.`)
                         })
                     }
                 } else if (content === 'create') {
